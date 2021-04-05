3e5e61a4a20be54131ff7dd399a061545d1de5e7
Nani <ih9137@naver.com>
2021-04-06 08:28:03 +0900

    챕터별 공부

---
+++ Chapter06/single-component.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="hello">
+		{{msg}}
+	</div>
+</template>
+
+<script>
+export default {
+	name: "hello",
+	data() {
+		return {
+			msg: "Vue js 앱에 오신걸 환형합니다."
+		}
+	}
+}
+</script>
